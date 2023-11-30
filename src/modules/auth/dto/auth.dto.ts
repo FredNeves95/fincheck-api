bf8d47025229d52800db212068672739ecd168cb
FredNevesAraujo <fredericoaraujo@fitec.org.br>
2023-11-30 16:54:46 -0300

    feat: auth routes and create user route to signup route

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MinLength, IsEmail } from 'class-validator';
+
+export class AuthDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -1,6 +1,6 @@
-import { IsNotEmpty, IsString, MinLength, IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class CreateUserDto {
+export class SignupDto {
   @IsString()
   @IsNotEmpty()
   name: string;
